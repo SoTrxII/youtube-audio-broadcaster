89fe2076e6f6a359d0907c86993a9178c3d4f6fa
SoTrxII <lo.lo.@lo.lo>
2024-02-27 17:25:10 +0100

    feat: Using a cache to reduce calls to ytdl.

--- server.js
+++ server.js
@@ -1,66 +1,91 @@
-
-const path = require("path");
-
-const express = require("express");
-const ytdl = require("@distube/ytdl-core");
+const redis = require('redis');
+const { PassThrough, Readable } = require('stream');
+const express = require('express');
+const ytdl = require('@distube/ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
-const logger = require('pino')( {level: 'debug'});
+const logger = require('pino')({ level: 'debug' });
 
-const expressLogger = require('pino-http')({logger: logger});
+const expressLogger = require('pino-http')({ logger });
 const sendSeekable = require('send-seekable');
-
+const YtMp3Stream = require('./services/decode-youtube');
 
 // Express settings
 
 const app = express();
 app.use(sendSeekable);
 app.use(expressLogger);
-
 app.set('port', process.env.PORT || 3000);
 
-app.get('/stream/:id', async function (request, response) {
-    const url = 'https://www.youtube.com/watch?v=' + request.params.id;
-    request.log.info(url);
-    try{
-        if(!await ytdl.validateURL(url)){
-            throw new Error(`Invalid youtube url : ${url}`);
-        }
-        const stream =  ytdl(url, { filter: 'audioonly' });
-        stream.on("info", (_, format) => {
-            logger.debug(`"${url}" content length : ${format.contentLength}`);
-            response.sendSeekable(stream, {
-                length: format.contentLength || 5000000
-            });
-        });
-    }catch (e){
-        logger.error(e);
-        response.statusCode = 400;
-        response.end("This isn't a correct Yt video link");
+const client = redis.createClient();
+client.on('connect', () => console.log('Connected to Redis...'));
+client.on('error', (err) => console.error(err));
+client.connect();
+
+const CACHE_EXPIRE = 4 * 3600;
+
+async function checkCache(req, res, next) {
+  const { id } = req.params;
+  try {
+    const data = await client.get(redis.commandOptions({ returnBuffers: true }), id);
+    if (data !== null) {
+      logger.info(`Id "${id}" found in Redis cache`);
+      res.setHeader('Content-type', 'audio/mpeg');
+      res.setHeader('Content-length', data.length);
+
+      const stream = new Readable();
+      stream.push(data);
+      stream.push(null);
+
+      res.sendSeekable(stream, {
+        length: data.length,
+      });
+    } else {
+      logger.info(`Id "${id}" not found in Redis cache`);
+      // Data not found in Redis cache, proceed to next middleware
+      next();
+    }
+  } catch (e) {
+    req.log.error(e);
+    res.statusCode = 500;
+    res.end('Error while trying to access cache');
+  }
+}
+app.get('/download/mp3/:id', checkCache, async (request, response) => {
+  const { id } = request.params;
+  const url = `https://www.youtube.com/watch?v=${id}`;
+  try {
+    const infos = await ytdl.getInfo(url);
+
+    const skipCache = infos?.videoDetails?.lengthSeconds > 3 * 3600 ?? false;
+    const cacheStream = new PassThrough(); // Stream to cache data
+    if (!skipCache) {
+      cacheStream.on('data', (chunk) => client.append(id, chunk));
+      cacheStream.on('error', async (e) => {
+        request.log.error(new Error('Error with Redis ', { cause: e }));
+        await client.del(id);
+      });
+      request.on('aborted', async () => {
+        logger.info('Request aborted, deleting from cache');
+        await client.del(id);
+      });
+      cacheStream.on('end', async () => {
+        logger.info('End triggered');
+        await client.expire(id, CACHE_EXPIRE);
+      });
     }
 
-    
+    ffmpeg(ytdl(url, { filter: 'audioonly' }))
+      .on('error', logger.error.bind(logger))
+      .toFormat('mp3')
+      .audioBitrate('192k')
+      .noVideo()
+      .pipe(cacheStream);
+    cacheStream.pipe(response);
+  } catch (e) {
+    response.statusCode = 400;
+    response.end("This isn't a correct Yt video link");
+    logger.error(e);
+  }
 });
 
-app.get('/download/mp3/:id', async function (request, response) {
-    request.log.debug('https://www.youtube.com/watch?v=' + request.params.id);
-    const url = 'https://www.youtube.com/watch?v=' + request.params.id;
-    try{
-        await ytdl.getInfo(url);
-        ffmpeg(ytdl(url, { filter: 'audioonly' }))
-            .on("error", console.error)
-            .toFormat('mp3')
-            .audioBitrate("192k")
-            .noVideo()
-            .pipe(response);
-    } catch (e) {
-        response.statusCode = 400;
-        response.end("This isn't a correct Yt video link");
-        logger.error(e);
-    }
-});
-
-app.listen(app.get('port'), function () {
-    logger.info('Started web server on port: ' + app.get('port'));
-});
-
-module.exports = {};
+app.listen(app.get('port'), () => logger.info(`Started web server on port: ${app.get('port')}`));
